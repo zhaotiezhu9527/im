530012804d7f78b3293cf862f55a57e9ad7a4d32
zhaotiezhu <zhaot0392@gmail.com>
2023-08-11 16:30:52 +0800

    关于我们

---
+++ api/src/main/java/com/juhai/api/controller/request/SiteConfigController.java
@@ -0,0 +1,43 @@
+package com.juhai.api.controller.request;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.juhai.commons.entity.MessageText;
+import com.juhai.commons.service.MessageTextService;
+import com.juhai.commons.service.ParamterService;
+import com.juhai.commons.utils.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Slf4j
+@Api(value = "获取系统配置相关", tags = "获取系统配置相关")
+@RequestMapping("/system")
+@RestController
+public class SiteConfigController {
+
+    @Autowired
+    private ParamterService paramterService;
+
+    @Autowired
+    private MessageTextService messageTextService;
+
+
+    @ApiOperation(value = "获取系统配置")
+    @GetMapping("/config")
+    public R config(HttpServletRequest httpServletRequest) {
+        Map<String, String> allParamByMap = paramterService.getAllParamByMap();
+        Map<String, MessageText> allMessageMap = messageTextService.getAllMessageMap();
+
+        JSONObject obj = new JSONObject();
+        obj.put("webDomain", allParamByMap.get("web_domain"));
+        obj.put("about", allMessageMap.get("about"));
+        return R.ok().put("data", obj);
+    }
+}
