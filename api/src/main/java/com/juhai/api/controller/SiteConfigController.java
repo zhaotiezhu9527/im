3684db8f01822f1609b34726c8aa343a1f2c4c29
zhaotiezhu <zhaot0392@gmail.com>
2023-08-24 13:12:41 +0700

    添加平台标识

--- api/src/main/java/com/juhai/api/controller/SiteConfigController.java
+++ api/src/main/java/com/juhai/api/controller/SiteConfigController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,9 @@ public class SiteConfigController {
     @Autowired
     private VersionService versionService;
 
+    @Value("${im.plat}")
+    private String plat;
+
 
     @ApiOperation(value = "获取系统配置")
     @GetMapping("/config")
@@ -45,6 +49,7 @@ public class SiteConfigController {
         JSONObject obj = new JSONObject();
         obj.put("webDomain", allParamByMap.get("web_domain"));
         obj.put("about", allMessageMap.get("about").getContent());
+        obj.put("plat", plat);
         return R.ok().put("data", obj);
     }
 
